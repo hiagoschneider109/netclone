6870116eb0fe0cf6b114839c04cb1c3846e0c6bf
Hiago Schneider <hiagoschneider109@icloud.com>
2022-06-11 17:26:17 +0200

    Add Nav Component

---
+++ src/components/Nav.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import { useState } from 'react';
+
+import './Nav.scss'
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import CardGiftcardIcon from '@material-ui/icons/CardGiftcard';
+import NotificationsIcon from '@material-ui/icons/Notifications'
+
+function Nav() {
+
+  const [navBlack, setNavBlack] = useState(false);
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  const transitionNav = () => {
+    window.scrollY > 100 ? setNavBlack(true) : setNavBlack(false);
+  };
+
+  useState(() => {
+    document.addEventListener("scroll", transitionNav);
+  });
+
+  const handleClick = () => {
+    console.log(toggleMenu);
+    toggleMenu ? setToggleMenu(false) : setToggleMenu(true);
+  };
+
+  //Scroll
+
+  //Scroll
+
+  return (
+    <div className={`nav ${navBlack && "nav--black"} ${toggleMenu && "show"}`}>
+        <button className='nav__burger' onClick={handleClick}>
+          <MenuIcon/>
+        </button>
+        <img src="./images/Logonetflix.png" className='nav__logo' alt="Netflix" />
+        <nav className='nav__links'>
+          <a href="/#" className="nav__link">
+            Accueil
+          </a>
+          <a href="/#" className="nav__link">
+            Séries
+          </a>
+          <a href="/#" className="nav__link">
+            Films
+          </a>
+        </nav>
+        <div className="nav__actions">
+          <a href="/#" className="nav__action">
+            <SearchIcon />
+          </a>
+          <a href="/*" className="nav__action">
+            DIRECT
+          </a>
+          <a href="/#" className="nav__action">
+            <CardGiftcardIcon />
+          </a>
+          <a href="/#" className="nav__action">
+            <NotificationsIcon />
+          </a>
+          <a href="/#" className="nav__action">
+            <img src="./images/Netflix-avatar.png" height={35} alt="AvatarUser" />
+          </a>
+          
+        </div>
+    </div>
+  );
+}
+
+export default Nav
