49fadebda54c7c161f78703993391281308bdc46
Hiago Schneider <hiagoschneider109@icloud.com>
2022-06-12 20:11:52 +0200

    Add Row Component

--- src/App.js
+++ src/App.js
@@ -3,20 +3,29 @@ import './App.scss';
 import Footer from './components/Footer';
 import Banner from './components/Banner';
 import Nav from './components/Nav'
+import Row from './components/Row';
+import requests from './config/requests';
 
 function App() {
   return (
     <div className="App">
-      {/* navbar */}
+
       <Nav />
 
       <Banner />
-
-      <div className="test"></div>
-      {/* banner */}
       
-      {/* rows */}
-      <div></div>
+      <Row 
+       title="Programmes originaux Netflix" 
+       fetchUrl = {requests.fetchNetflixOriginals}
+       isPoster={true}
+      />
+      <Row title="Tendances actuelles" fetchUrl={requests.fetchTrending} />
+      <Row title="Les mieux notés" fetchUrl={requests.fetchTopRated}/>
+      <Row title="Films d'action" fetchUrl={requests.fetchActionMovies}/>
+      <Row title="Films d'horreur" fetchUrl={requests.fetchHorrorMovies}/>
+      <Row title="Comédies" fetchUrl={requests.fetchComedyMovies}/>
+      <Row title="Documentaires" fetchUrl={requests.fetchDocumentaries}/>
+
       {/* video */}
 
       {/* quick view */}
